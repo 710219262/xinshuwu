e683663138c705380182bdc61e30a7c088654bca
710219262qq.com <710219262@qq.com>
2019-10-04 17:00:50 +0800

    测试好

--- routes/mms.php
+++ routes/mms.php
@@ -14,6 +14,7 @@
 $router->group(['namespace' => 'MMS', 'prefix' => 'mms'], function () use ($router) {
     $router->get('test', 'Index@userList');//测试用户列表
 
+
     $router->get('tests', 'Index@feedBack');//测试用户列表
     $router->post('auth', 'Common@auth');
     $router->post('authpass', 'Common@authpass');
